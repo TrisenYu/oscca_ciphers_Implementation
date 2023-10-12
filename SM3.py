68d8a5896c0e9d88bc1c157e405a6ae2bc68dabb
trisenyu <kisfg@hotmail.com>
2023-10-13 00:42:26 +0800

    隐藏 SM3 的输入。

--- SM3.py
+++ SM3.py
@@ -4,9 +4,9 @@
 :cite:https://oscca.gov.cn/sca/xxgk/2010-12/17/1002389/files/302a3ada057c4a73830536d03e683110.pdf
 
     ┌──[case1]: `abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd`
-    └─> 0xdebe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732
+    ├─> 0xdebe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732
     │
-    ┌──[case2]: `abc`
+    ├──[case2]: `abc`
     └─> 0x66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0
 """
 
@@ -86,51 +86,51 @@ def CompressFunction(_idx: int, _V: int, _B: str) -> int:
     return tmp ^ _V
 
 
-def initPlaintext(isNeed: bool = False, para_str: str = "") -> str:
-    msg = input() if not isNeed else para_str
-
-    _bin, k_4_0, st = '', 0, 0
-
-    for c in msg:  # 固定为 unicode 编码。
-        _ = bin(ord(c))[2:]
-        while len(_) % 8 != 0:  # 不会自动补成 8 的倍数
-            _ = '0' + _
-        _bin += _
-
-    _bin_length = len(_bin)
-
-    while True:  # l + k_(0) = 447 + 512k, l > 0
-        _ = 447 + 512 * st - _bin_length
-        if _ >= 0:
-            k_4_0 = _
-            break
-        st += 1
-
-    _bin += '1'
-    for _ in range(0, k_4_0):
-        _bin += '0'
-
-    _remainBits, temp = bin(_bin_length)[2:], ''
-    _len = len(_remainBits)
-    _lst_check = 64 - _len
-
-    if _lst_check < 0:
-        print("[长度过长]")
-        exit(0)
-
-    for _ in range(0, _lst_check):  # 需填充至 64 位
-        temp += '0'
-    _bin += temp + _remainBits
-    return _bin
-
-
 def SM3Hash(isNeed: bool = False, para_str: str = "") -> str:
     """
     SM3Hash 入口。
-    :param isNeed: 不需要则手动输入。以后应能支持任意字节输入。
-    :param para_str: 可能的目的参数。
-    :return: sm3 十六进制杂凑值。
+        :param isNeed: 不需要则手动输入。以后应能支持任意字节输入。
+        :param para_str: 可能的目的参数。
+        :return: sm3 十六进制杂凑值。
     """
+    
+    def initPlaintext(_isNeed: bool = False, _para_str: str = "") -> str:
+        msg = input() if not _isNeed else _para_str
+
+        _bin, k_4_0, st = '', 0, 0
+
+        for c in msg:  # 固定为 unicode 编码。
+            _ = bin(ord(c))[2:]
+            while len(_) % 8 != 0:  # 不会自动补成 8 的倍数
+                _ = '0' + _
+            _bin += _
+
+        _bin_length = len(_bin)
+
+        while True:  # l + k_(0) = 447 + 512k, l > 0
+            _ = 447 + 512 * st - _bin_length
+            if _ >= 0:
+                k_4_0 = _
+                break
+            st += 1
+
+        _bin += '1'
+        for _ in range(0, k_4_0):
+            _bin += '0'
+
+        _remainBits, temp = bin(_bin_length)[2:], ''
+        _len = len(_remainBits)
+        _lst_check = 64 - _len
+
+        if _lst_check < 0:
+            print("[长度过长]")
+            exit(0)
+
+        for _ in range(0, _lst_check):  # 需填充至 64 位
+            temp += '0'
+        _bin += temp + _remainBits
+        return _bin
+
     plaintext_bin = initPlaintext(isNeed, para_str)
     dataChunkB, num, Vu, l_ptr, r_limit = [], len(plaintext_bin) // 512, [IV], 1, len(plaintext_bin)
 
@@ -147,8 +147,4 @@ def SM3Hash(isNeed: bool = False, para_str: str = "") -> str:
     return hex(Vu[num])
 
 
-"""
-if __name__ == "__main__":
-    tester = SM3Hash()
-    print(tester)
-"""
+# print(SM3Hash())
